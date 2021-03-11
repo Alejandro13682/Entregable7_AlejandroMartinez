965eaf05acb45deb477b514069a4fdb166050055
Alejandro13682 <68309225+Alejandro13682@users.noreply.github.com>
2021-03-11 19:24:15 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const express = require('express')
 const app = express()
 const fs = require('fs')
-app.use(express.json())
 const puerto = 8080
 const productos = fs.readFileSync('./productos.txt', 'utf-8')
 const objProd = JSON.parse(productos)
 let cant = objProd.length
 let Itemvisitas = 0
 let itemRandomVisitas = 0
-
 const productoRandom = () => {
     let arrID = objProd.map(item => item.id)
     let randomID = Math.floor(Math.random() * arrID.length + 1)
