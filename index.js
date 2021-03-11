9cceea44f508bfd5f0633b1a3b00fbebfd4cf488
Alejandro13682 <68309225+Alejandro13682@users.noreply.github.com>
2021-03-10 23:12:32 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const fs = require('fs')
+app.use(express.json())
+const puerto = 8080
+const productos = fs.readFileSync('./productos.txt', 'utf-8')
+const objProd = JSON.parse(productos)
+let cant = objProd.length
+let Itemvisitas = 0
+let itemRandomVisitas = 0
+const productoRandom = () => {
+    let arrID = objProd.map(item => item.id)
+    let randomID = Math.floor(Math.random() * arrID.length + 1)
+    let productoRandom = objProd.filter(producto => producto.id == randomID)
+    return productoRandom
+}
+productoRandom()
+
+app.get('/items', (req, res) => {
+    Itemvisitas += 1
+    res.send({items: objProd, cantidad: (cant)})    
+})
+
+app.get('/item-random', (req, res) => {
+    itemRandomVisitas += 1
+    res.send({item: productoRandom()})
+})
+
+
+app.get('/visitas', (req, res) => {    
+    res.send({visitas: {items: Itemvisitas, item: itemRandomVisitas}})    
+})
+
+
+//Escuchamos los cambios en el puerto
+
+app.listen(puerto , ()=> {
+    console.log('El servidor esta funcionando')
+})
+app.on('error', error => console.log(`Error en el puerto ${puerto}`))
+
